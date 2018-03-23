adcc7a68e49f3b290377be5f32ad96c34e19673d
Arun <arunstar333@gmail.com>
2018-03-23 18:06:55 +0530

    update shell for rename filename

--- commands.sh
+++ commands.sh
@@ -1,3 +1,11 @@
 # Create new local branch
 git checkout -b <branch>
 http://dont-be-afraid-to-commit.readthedocs.io/en/latest/git/commandlinegit.html
+
+
+#Rename all hyphen to underscore 
+for i in *-*;do mv $i ${i//"-"/"_"};done
+#https://gist.github.com/fhferreira/f429a059b9c1efcfa656
+
+#How to rename all folders and files to lowercase on Linux?
+find my_root_dir -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
